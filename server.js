d5cbc25c572007c7e770e31d17d5dc11385d700b
Lyndon <lyndon.stluce@gmail.com>
2024-09-18 22:24:03 -0400

    Switch from bcrypt to bcryptjs

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Import dependencies
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { Pool } = require('pg');
 const cors = require('cors');
